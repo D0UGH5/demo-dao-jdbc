59edf37ac780b73335a9857a32eb042e5dadecf7
Murilo Furtado <you@example.commurilofneto1@gmail.com>
2026-01-29 21:18:05 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import db.DB;
 import db.DbException;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 import java.sql.SQLException;
@@ -11,6 +12,14 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         try {
             return new SellerDaoJDBC(DB.getConnection());
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static DepartmentDao createDepartmentDao(){
+        try {
+            return new DepartmentDaoJDBC(DB.getConnection());
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
